30d7f5aa53ffa654a9911fb7569335fc73089464
NaimurRahmann <niamurreiman@gmail.com>
2026-07-09 15:27:07 +0600

    feat: Add llm model

--- app/main.py
+++ app/main.py
@@ -1,14 +1,18 @@
 from fastapi import FastAPI
 from app.core.config import settings
+from app.llm import get_llm
 app=FastAPI(
     title=settings.app_name,
     description="AI-powered travel planning assistant",
     version=settings.app_version
 )
+llm=get_llm()
 @app.get("/")
 def root():
+    response=llm.invoke("say hello in one sentence")
     return {"message": "Travel AI API is running",
             "model":settings.gemini_model,
+            "response":response
             }
 
 @app.get("/health")
